01ded4708483759d4f8386f768019fa02ff010ae
Simon Greter <greter@UX31A.(none)>
2013-04-04 23:45:55 +0200

    switch to pointer as a prep for also supporting HardwareSerial

--- ArduinoSerialEcho.ino
+++ ArduinoSerialEcho.ino
@@ -2,19 +2,20 @@
 
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(50, 51); // RX, TX
+SoftwareSerial* pSerial = new SoftwareSerial(50, 51);
+//SoftwareSerial mySerial(50, 51); // RX, TX
 
 void setup()  
 {
-  mySerial.begin(38400);
-  mySerial.println("Port initialized");
+  pSerial->begin(38400);
+  pSerial->println("Port initialized");
 }
 
 void loop() // run over and over
 {
-  if(mySerial.available())
+  if(pSerial->available())
   {
-    mySerial.write(mySerial.read());
+    pSerial->write(pSerial->read());
   }
 }
-
+  
