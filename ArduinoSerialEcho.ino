6ec6005303251874fe5613435740b45452313bfd
Simon Greter <greter@UX31A.(none)>
2013-04-04 23:06:34 +0200

    comment added

--- ArduinoSerialEcho.ino
+++ ArduinoSerialEcho.ino
@@ -1,1 +1,20 @@
+// Open a serial port ad simply return every character
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(50, 51); // RX, TX
+
+void setup()  
+{
+  mySerial.begin(38400);
+  mySerial.println("Port initialized");
+}
+
+void loop() // run over and over
+{
+  if(mySerial.available())
+  {
+    mySerial.write(mySerial.read());
+  }
+}
 
