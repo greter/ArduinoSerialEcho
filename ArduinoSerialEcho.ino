b140c1b55fdb8f5f84da6669b65dda01df049664
Simon Greter <greter@UX31A.(none)>
2013-04-05 00:06:49 +0200

    comments added

--- ArduinoSerialEcho.ino
+++ ArduinoSerialEcho.ino
@@ -1,21 +1,19 @@
-// Open a serial port ad simply return every character
+// Open a serial port and just return every character that is sent to the the port
 
 #include <SoftwareSerial.h>
 
-SoftwareSerial* pSerial = new SoftwareSerial(50, 51);
-//SoftwareSerial mySerial(50, 51); // RX, TX
-
+SoftwareSerial* pSerial = new SoftwareSerial(50, 51); // define the RX and TX ports, refere to chapter limitations on http://arduino.cc/en/Reference/SoftwareSerial for a list of pins that work with your board
 void setup()  
 {
-  pSerial->begin(38400);
+  pSerial->begin(38400); // set the baudrate, on an Mega 2560 Board 38400 Baud seams to be the max working speed
   pSerial->println("Port initialized");
 }
 
 void loop() // run over and over
 {
-  if(pSerial->available())
+  if(pSerial->available()) // check if new data is available for reading
   {
-    pSerial->write(pSerial->read());
+    pSerial->write(pSerial->read()); // read data and send it imediatly back
   }
 }
   
