4a0f994fd15fa374d60505d96b69906219539dfd
Simon Greter <greter@UX31A.(none)>
2013-04-05 22:53:04 +0200

    support for HW-Serial ports added

--- ArduinoSerialEcho.ino
+++ ArduinoSerialEcho.ino
@@ -2,10 +2,14 @@
 
 #include <SoftwareSerial.h>
 
-SoftwareSerial* pSerial = new SoftwareSerial(50, 51); // define the RX and TX ports, refere to chapter limitations on http://arduino.cc/en/Reference/SoftwareSerial for a list of pins that work with your board
+// use on of the following lines to either run the code with a HW- or SW-Serial port
+
+//SoftwareSerial* pSerial = new SoftwareSerial(50, 51); // define the RX and TX ports, refere to chapter limitations on http://arduino.cc/en/Reference/SoftwareSerial for a list of pins that work with your board
+HardwareSerial* pSerial = &Serial1; // assign the decired HW-Serial port
+
 void setup()  
 {
-  pSerial->begin(38400); // set the baudrate, on an Mega 2560 Board 38400 Baud seams to be the max working speed
+  pSerial->begin(115200); // set the baudrate, on an Mega 2560 Board 38400 Baud seams to be the max working speed for SoftwareSerial
   pSerial->println("Port initialized");
 }
 
